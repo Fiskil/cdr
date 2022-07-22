ce54159ce24fdff3aacefdfc58ab56b7dab17c0e
Banner B. Schafer <bannerbschafer@fastmail.com>
2022-07-22 14:41:34 +1000

    removing unused module

--- data/wkc_endpoints.go
+++
@@ -1,8 +0,0 @@
-// Package data provides some (mostly) static data that can be useful for getting started with CDR without tracking down a whole bunch of random constants.
-package data
-
-const (
-	CDRBaseSecureURL = "https://secure.api.cdr.gov.au"
-	CDRBaseURL       = "https://api.cdr.gov.au"
-	BankAustBaseURL  = "https://identity-mtls.cdr-api.bankaust.com.au"
-)
